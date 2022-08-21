4ed4febc4c8d585155728a391f9268461f4ac445
Tom Forsyth <thomas.forsyth@stonybrook.edu>
2022-08-20 20:27:24 -0400

    Removed UnrealSim code

--- MotionPlanner/MotionPlanner.Build.cs
+++
@@ -1,9 +0,0 @@
-using UnrealBuildTool;
-
-public class MotionPlanner: ModuleRules
-{
-    public MotionPlanner(ReadOnlyTargetRules Target) : base(Target) 
-    {
-        PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine"}); 
-    }
-}
